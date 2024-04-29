b635a3599dd7b6b40d47f6d4e1db39659c16a3d8
sikorskayaX <sikorskaya02@gmail.com>
2024-04-30 02:24:30 +0300

    Add character-details.js, connect character with local storage

---
+++ scripts/character-details.js
@@ -0,0 +1,31 @@
+﻿const characterMain = document.getElementById('character__main');
+const characterProperties = document.getElementById('character__properties');
+const selectedCharacterId = localStorage.getItem('selectedCharacterId');
+/*characterProperties.innerHTML = ''; */
+
+
+function loadCharacter() {
+    axios.get(`https://rickandmortyapi.com/api/character/${selectedCharacterId}`)
+    .then(function (response) {
+        const character = response.data;
+        showCharacter(character);
+        console.log(character)
+    })
+    .catch(function (error) {
+        console.error(error);
+    });  
+}    
+
+function showCharacter(character){
+    characterMain.innerHTML = ''; 
+    const characterPropertiesHead = document.createElement('div');
+    characterPropertiesHead.classList.add('character__head');
+    characterPropertiesHead.innerHTML = `
+        <img src="${character.image}" alt="${character.name}" class="character__image">
+        <h1>${character.name}</h1>
+        `;
+    characterMain.appendChild(characterPropertiesHead);
+}
+
+
+window.onload = loadCharacter;
---
+++ scripts/character.js
@@ -0,0 +1,67 @@
+﻿const charactersContainer = document.getElementById('characters');
+const loadButton = document.getElementById('load');
+charactersContainer.innerHTML = ''; 
+let currentPage = 1;
+let characters = [];
+
+const characterProperties = document.getElementById('character__properties');
+console.log(characterProperties)
+
+function loadCharacters(page = 1) {
+    axios.get(`https://rickandmortyapi.com/api/character?page=${page}`)
+        .then(function (response) {
+            characters = response.data.results; 
+            showCharacters(characters);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+        
+}
+
+function showCharacters(characters){
+    characters.forEach(character => {
+        const characterElement = document.createElement('a');
+        characterElement.classList.add('characters__container');
+        characterElement.href = "../pages/character-details.html";
+        characterElement.innerHTML = `
+            <img class="characters__image" src="${character.image}" alt="${character.name}">
+            <h6 class="characters__name">${character.name}</h6>
+            <p class="characters__species regular">Вид: ${character.species}</p>
+        `;
+        charactersContainer.appendChild(characterElement);
+        characterElement.addEventListener('click', () => {
+          localStorage.setItem('selectedCharacterId', character.id);
+      });
+    });
+}
+
+window.onload = () => {
+  loadCharacters(1);
+  charactersContainer.style.display = 'flex';
+};
+
+
+loadButton.addEventListener('click', event => {
+  console.log('LOAD MORE', event.type);
+  charactersContainer.style.display = 'flex';
+  currentPage++;
+  loadCharacters(currentPage);
+});
+
+
+
+const filterInput = document.getElementById('filter-name');
+
+
+filterInput.addEventListener('input', () => {
+    const filteredCharacters = characters.filter(character =>
+      character.name.toLowerCase().includes(filterInput.value.toLowerCase())
+    );
+    charactersContainer.innerHTML = ''; 
+    showCharacters(filteredCharacters)
+  });
+
+  charactersContainer.addEventListener('click', () => {
+    console.log('click');
+  });
