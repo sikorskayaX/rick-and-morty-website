94ab6de2b2f29d74e7309c5c8f24665f0413220c
sikorskayaX <sikorskaya02@gmail.com>
2024-04-29 21:14:08 +0300

    Refactor character listing and add dynamic loading. The static list of characters has been replaced with a dynamic loading mechanism using the Rick and Morty API. A new script.js file has been created to handle the API requests and populate the character data dynamically on page load.

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+﻿function loadCharacters() {
+    axios.get('https://rickandmortyapi.com/api/character')
+        .then(function (response) {
+            const characters = response.data.results; 
+            const charactersContainer = document.getElementById('characters');
+            charactersContainer.innerHTML = ''; 
+
+            characters.forEach(character => {
+                const characterElement = document.createElement('div');
+                characterElement.classList.add('characters__container');
+                characterElement.innerHTML = `
+                    <img class="characters__image" src="${character.image}" alt="${character.name}">
+                    <h6 class="characters__name">${character.name}</h6>
+                    <p class="characters__species regular">Вид: ${character.species}</p>
+                `;
+                charactersContainer.appendChild(characterElement);
+            });
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+}
+
+window.onload = loadCharacters;
