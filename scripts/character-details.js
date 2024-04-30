144181d01533014c3e9fcedc53ec06a6bbda8c6c
sikorskayaX <sikorskaya02@gmail.com>
2024-04-30 03:03:11 +0300

    Refactor character details HTML and JS for add properties of characters

--- scripts/character-details.js
+++ scripts/character-details.js
@@ -16,16 +16,48 @@ function loadCharacter() {
     });  
 }    
 
-function showCharacter(character){
-    characterMain.innerHTML = ''; 
-    const characterPropertiesHead = document.createElement('div');
-    characterPropertiesHead.classList.add('character__head');
+function createElementWithText(tag, textContent, className) {
+    const element = document.createElement(tag);
+    element.textContent = textContent;
+    if (className) {
+        element.classList.add(className);
+    }
+    return element;
+}
+
+function showCharacter(character) {
+    characterMain.innerHTML = '';
+
+    const characterPropertiesHead = createElementWithText('div', '', 'character__head');
     characterPropertiesHead.innerHTML = `
         <img src="${character.image}" alt="${character.name}" class="character__image">
         <h1>${character.name}</h1>
-        `;
+    `;
     characterMain.appendChild(characterPropertiesHead);
+
+    const characterInformations = createElementWithText('div', '', 'character__informations');
+    const characterInformationsTitle = createElementWithText('h2', 'Informations');
+    characterInformations.appendChild(characterInformationsTitle);
+
+    const properties = [
+        { label: 'Gender', value: character.gender },
+        { label: 'Status', value: character.status },
+        { label: 'Species', value: character.species },
+        { label: 'Origin', value: character.origin.name },
+        { label: 'Type', value: character.type ? character.type.name : 'Unknown' }, 
+        { label: 'Location', value: character.location.name }
+    ];
+
+    properties.forEach(prop => {
+        const propertyTitle = createElementWithText('h3', prop.label);
+        const propertyValue = createElementWithText('p', prop.value);
+        characterInformations.appendChild(propertyTitle);
+        characterInformations.appendChild(propertyValue);
+    });
+
+    characterMain.appendChild(characterInformations);
 }
+
 
 
 window.onload = loadCharacter;
